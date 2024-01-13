98c37cf45ebf047017d5c1034de6dd3de54805d3
Priyansh <guptapriyansh097@gmail.com>
2024-01-13 19:52:42 +0530

    update

--- add_remote.bat
+++ add_remote.bat
@@ -1,18 +1,19 @@
-
+@echo off
+:: Initialize Git repository
 git init
 
+:: Add and commit changes to the main branch
 git add .
 git commit -m "update"
 
-
+:: Create and switch to the contribute branch
 git checkout -b contribute
 
+:: Add remote repository with branch-specific URL
+git remote add Priyansh-666 https://github.com/Priyansh-666/auto-gitter.git
 
-git remote add Priyansh-666 https://github.com/Priyansh-666/auto-gitter
-
-
+:: Fetch and rebase changes from the remote contribute branch
 git pull --rebase Priyansh-666 contribute
-git rebase --continue
 
-
+:: Push changes to the remote contribute branch
 git push Priyansh-666 contribute
