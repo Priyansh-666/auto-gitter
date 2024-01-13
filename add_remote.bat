3f6dc832d84041ba926e3f291d2504eedf082fb1
Priyansh <guptapriyansh097@gmail.com>
2024-01-13 20:00:16 +0530

    update

--- add_remote.bat
+++ add_remote.bat
@@ -5,7 +5,7 @@ git init
 :: Add and commit changes to the main branch
 git add .
 git commit -m "update"
-
+git branch remotebat
 
 :: Add remote repository with branch-specific URL
 git remote add Priyansh-666 https://github.com/Priyansh-666/auto-gitter.git
