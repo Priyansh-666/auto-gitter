88a6dbbbb0cda1488947de11d96c300966203e21
Priyansh <guptapriyansh097@gmail.com>
2024-01-13 19:47:15 +0530

    update

--- add_remote.bat
+++ add_remote.bat
@@ -1,13 +1,9 @@
 
-    git init
-    git add .
-    git commit -m "update"
-    git branch contribute
-    git remote add Priyansh-666 https://github.com/Priyansh-666/auto-gitter
-    git fetch Priyansh-666
-    git checkout contribute
-    git rebase Priyansh-666/contribute
-    git rebase --continue
-    
-    git push Priyansh-666 contribute
-    
+git init
+git add .
+git commit -m "update"
+git checkout -b contribute
+git remote add Priyansh-666 https://github.com/Priyansh-666/auto-gitter
+git fetch Priyansh-666
+git rebase Priyansh-666/contribute
+git push Priyansh-666 contribute
